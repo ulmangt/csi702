1ac23420bee786ad52dc83760e53d04e6e610bc5
geoff.ulman <geoff.ulman@20f29d02-21cd-11df-99a0-0961c9d9be7b>
2010-03-07 04:07:38 +0000

    Added convert.c and convert.h for unit conversion functions.

---
+++ hdr/convert.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define PI acos(-1)
+#define RAD_TO_DEG 90.0 / PI
+#define DEG_TO_RAD 1.0 / RAD_TO_DEG
+
+float toRadians( float );
+float toDegrees( float );
