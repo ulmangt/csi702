3a6f26adfe93d3109181d25c6fa01e0341b13954
geoff.ulman <geoff.ulman@20f29d02-21cd-11df-99a0-0961c9d9be7b>
2010-03-07 04:39:20 +0000

    Continued improving unit conversion routines. Added function to convert to navy degrees.

--- trunk/hdr/convert.h
+++ trunk/hdr/convert.h
@@ -2,11 +2,12 @@
 #define CONVERT_H_INCLUDED
 
 #define PI acos(-1)
-#define RAD_TO_DEG 90.0 / PI
+#define RAD_TO_DEG 180.0 / PI
 #define DEG_TO_RAD 1.0 / RAD_TO_DEG
 
 float toRadians( float );
 float toDegrees( float );
+float toNavyDegrees( float );
 float fromRadians( float );
 float fromDegrees( float );
 
