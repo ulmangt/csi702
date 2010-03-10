01f7d65e3650ebba46681494b804872b802b8573
geoff.ulman <geoff.ulman@20f29d02-21cd-11df-99a0-0961c9d9be7b>
2010-03-10 06:20:51 +0000

    Added functions to handle creation of errored observations based on ground truth waypoint lists.

--- hdr/obs_math.h
+++ hdr/obs_math.h
@@ -1,6 +1,8 @@
 #ifndef OBS_MATH_H_INCLUDED
 #define OBS_MATH_H_INCLUDED
 
+
+
 // used to describe ownship and target motion
 // as a series of time, x, y tuples
 struct waypoint
@@ -16,8 +18,42 @@ struct waypoint_list
   struct waypoint* waypoints;
 };
 
+enum observation_type
+{
+  AZIMUTH = 1,
+  RANGE,
+  SPEED,
+};
+
+struct observation
+{
+  // observation_type (determines how value is interpreted)
+  int type;
+  // the timestamp of the observation
+  float time;
+  // x position of sensor
+  float x_pos;
+  // y position of sensor
+  float y_pos;
+  // observation value
+  float value;
+  // observation error
+  float error;
+};
+
+struct observation_list
+{
+  int size;
+  struct observation* observations;
+};
+
+struct observation_list *generate_observations( struct waypoint_list*, struct waypoint_list*, int, float, float, float, float );
+float generate_observation( int, float, float, float, float, float );
+float generate_azimuth_observation( float, float, float, float, float );
+
 int interpolate( struct waypoint_list* , float, float*, float* );
 int interpolate_waypoints( struct waypoint*, struct waypoint*, float, float*, float*);
+
 float azimuth( float, float, float, float );
 
 #endif
