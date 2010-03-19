16553320ee8fa8fba943cfe48d238f33d704bcde
geoff.ulman <geoff.ulman@20f29d02-21cd-11df-99a0-0961c9d9be7b>
2010-03-19 23:38:22 +0000

    homework4: greatly simplified load_particles()

--- branches/homework4/src/problem3/nbody_serial.c
+++ branches/homework4/src/problem3/nbody_serial.c
@@ -5,35 +5,24 @@
 
 #include "nbody_util.h"
 
-#define PARTICLES_PER_PROC 10
-#define NUM_PROC 10
-
 int main( )
 {
   int i;
 
-  float **x_proc = malloc( sizeof( float* ) * NUM_PROC );
-  float **y_proc = malloc( sizeof( float* ) * NUM_PROC );
-  float **z_proc = malloc( sizeof( float* ) * NUM_PROC );
+  float *x_proc = malloc( sizeof( float ) * NUM_PROC * PARTICLES_PER_PROC );
+  float *y_proc = malloc( sizeof( float ) * NUM_PROC * PARTICLES_PER_PROC );
+  float *z_proc = malloc( sizeof( float ) * NUM_PROC * PARTICLES_PER_PROC );
 
   for ( i = 0 ; i < NUM_PROC ; i++ )
   {
-    load_particles( get_particle_file_name( i ), x_proc+i, y_proc+i, z_proc+i );
+    int shift = i * PARTICLES_PER_PROC;
+    load_particles( get_particle_file_name( i ), x_proc + shift , y_proc + shift, z_proc + shift );
   }
 
-  for ( i = 0 ; i < NUM_PROC ; i++ )
+  for ( i = 0 ; i < NUM_PROC * PARTICLES_PER_PROC ; i++ )
   {
-    printf( "file: %s\n" , get_particle_file_name( i ) );
-
-    float *x_pos = *( x_proc + i );
-    float *y_pos = *( y_proc + i );
-    float *z_pos = *( z_proc + i );
-
-    int j;
-    for ( j = 0 ; j < PARTICLES_PER_PROC ; j++ )
-    {
-      printf( "%f %f %f\n", x_pos[j], y_pos[j], z_pos[j] );
-    }
+    printf("%d %f %f %f\n", i, x_proc[i], y_proc[i], z_proc[i]);
   }
-
 }
+
+
