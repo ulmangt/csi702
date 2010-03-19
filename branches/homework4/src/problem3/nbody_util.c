cbac8bea2cd218bee49dea7d51ff52e7848bf2b3
geoff.ulman <geoff.ulman@20f29d02-21cd-11df-99a0-0961c9d9be7b>
2010-03-19 22:48:24 +0000

    homework4: decomposed code into a utilities file nbody_util.h

---
+++ branches/homework4/src/problem3/nbody_util.c
@@ -0,0 +1,54 @@
+
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+// generate a random float between 0 and 1
+float frand( )
+{
+  return ( float ) rand( ) / ( float ) RAND_MAX ;
+}
+
+void write_particles( char *file_name, int num_particles )
+{
+  FILE *file = fopen( file_name, "w" );
+
+  int count;
+
+  // first line of the file contains the total number of particles
+  fprintf( file, "%d\n", num_particles );
+
+  for ( count = 0 ; count < num_particles ; count++ )
+  {
+    fprintf( file, "%f %f %f\n", frand( ), frand( ), frand( ) );
+  }
+  
+  close( file );
+}
+
+void load_particles( char *file_name, float *x_pos, float *y_pos, float *z_pos )
+{
+  FILE *file = fopen( file_name, "r" );
+  
+  int total, count;
+  int success = fscanf( file, "%d", &total );
+
+  if ( success == EOF ) {
+    close( file );
+    return;
+  }
+
+  x_pos = malloc( sizeof( float ) * total );
+  y_pos = malloc( sizeof( float ) * total );
+  z_pos = malloc( sizeof( float ) * total );
+
+  for ( count = 0 ; count < total ; count++ )
+  {
+    int success = fscanf( file, "%f%f%f" , x_pos++, y_pos++, z_pos++ );
+
+    if ( success == EOF )
+      break;
+  }
+
+  close( file );
+}
