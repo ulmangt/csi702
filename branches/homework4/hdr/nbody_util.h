b23ba0523c272eb42814344105c3330ec86b682e
geoff.ulman <geoff.ulman@20f29d02-21cd-11df-99a0-0961c9d9be7b>
2010-03-20 02:07:16 +0000

    homework4: potentials are now written out to a file by each node. This won't work when we run for real on remote nodes, but it is useful for testing.

--- branches/homework4/hdr/nbody_util.h
+++ branches/homework4/hdr/nbody_util.h
@@ -8,6 +8,7 @@ float frand( );
 float distance( float, float, float, float, float, float );
 
 char* get_particle_file_name( int );
+char* get_file_name( char*, int );
 
 void write_particles( char*, int );
 void write_potentials( char*, int num_particles, float* );
