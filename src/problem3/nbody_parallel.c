398ee37bcb76d8a4ec579256075c773ff3fb1dff
geoff.ulman <geoff.ulman@20f29d02-21cd-11df-99a0-0961c9d9be7b>
2010-03-20 01:07:05 +0000

    homework4: cartesian comm group fully working.

--- src/problem3/nbody_parallel.c
+++ src/problem3/nbody_parallel.c
@@ -9,7 +9,7 @@
 
 int main( int argc, char** argv )
 {
-  int numprocs, myid, mycartid;
+  int numprocs, myid;
 
   MPI_Init( &argc, &argv );
   MPI_Comm_size( MPI_COMM_WORLD, &numprocs );
@@ -17,26 +17,36 @@ int main( int argc, char** argv )
 
   printf( "id %d of %d\n", myid, numprocs );
 
-  MPI_Comm cart_comm;
-
   // create a 1 dimensional periodic cartesian grid
   // with numprocs nodes and store in cart_comm
-  MPI_Cart_create( MPI_COMM_WORLD, 1, numprocs, 1, 1, &cart_comm );
+  MPI_Comm cart_comm;
+  int periods = 1;
+  MPI_Cart_create( MPI_COMM_WORLD, 1, &numprocs, &periods, 1, &cart_comm );
 
   // get rank in the new comm group
+  int mycartid;
   MPI_Comm_rank( cart_comm, &mycartid );
-  
-  int source, topid, bottomid;
 
-  // get our top neighbor
-  MPI_Cart_shift( cart_comm, 0, 1, &source, &topid );
+  // get our neighbors ( 1 space away in the 0th cartesian dimension)
+  int bottom, top;
+  MPI_Cart_shift( cart_comm, 0, 1, &bottom, &top );
 
-  // get our bottom neighbor
-  MPI_Cart_shift( cart_comm, 0, -1, &source, &bottomid );  
+  // get our coordinates in the cartesian grid
+  int coords;
+  MPI_Cart_coords( cart_comm, mycartid, 1, &coords );
+
+  if ( coords % 2 == 0 )
+  {
+    printf("even\n");
+  }
+  else
+  {
+    printf("odd\n");
+  }
 
   void * buf = malloc( sizeof(int) );
-
-  printf( "id %d top neighbor %d bottom neighbor %d\n", source, topid, bottomid );
+  
+  
 
   MPI_Finalize();
 
