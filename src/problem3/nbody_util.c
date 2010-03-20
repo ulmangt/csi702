9f3771e0c6f03bf68728c7a1a59a006e16519d63
geoff.ulman <geoff.ulman@20f29d02-21cd-11df-99a0-0961c9d9be7b>
2010-03-20 00:11:18 +0000

    homework4: finished nbody_serial code

--- src/problem3/nbody_util.c
+++ src/problem3/nbody_util.c
@@ -11,6 +11,13 @@ float frand( )
   return ( float ) rand( ) / ( float ) RAND_MAX ;
 }
 
+// calculates the distance between two particles
+float distance( float x1, float y1, float z1, float x2, float y2, float z2 )
+{
+  return sqrt( pow( x1 - x2 , 2.0 ) + pow( y1 - y2 , 2.0 ) + pow( z1 - z2 , 2.0 ) ) ;
+}
+
+// returns the name of the particle data file for the num process
 char *get_particle_file_name( int num )
 {
   char *file_name = malloc( sizeof(char) * 100 );
@@ -18,6 +25,23 @@ char *get_particle_file_name( int num )
   return file_name;
 }
 
+// writes the particles in the provided arrays to file_name
+void write_potentials( char *file_name, int num_particles, float *potential )
+{
+  FILE *file = fopen( file_name, "w" );
+
+  int count;
+
+  for ( count = 0 ; count < num_particles ; count++ )
+  {
+    fprintf( file, "%d %f\n", count, potential[count] );
+  }
+  
+  close( file );
+}
+
+
+// writes a random set of num_particles to file_name
 void write_particles( char *file_name, int num_particles )
 {
   FILE *file = fopen( file_name, "w" );
@@ -32,35 +56,7 @@ void write_particles( char *file_name, int num_particles )
   close( file );
 }
 
-/*
-void load_particles( char *file_name, float **x_pos, float **y_pos, float **z_pos )
-{
-  FILE *file = fopen( file_name, "r" );
-  
-  int total, count;
-  int success = fscanf( file, "%d", &total );
-
-  if ( success == EOF ) {
-    close( file );
-    return;
-  }
-
-  *x_pos = malloc( sizeof( float ) * total );
-  *y_pos = malloc( sizeof( float ) * total );
-  *z_pos = malloc( sizeof( float ) * total );
-
-  for ( count = 0 ; count < total ; count++ )
-  {
-    int success = fscanf( file, "%f%f%f" , (*x_pos) + count, (*y_pos) + count, (*z_pos) + count );
-
-    if ( success == EOF )
-      break;
-  }
-
-  close( file );
-}
-*/
-
+// loads PARTICLES_PER_PROC particles from file_name into the provided arrays
 void load_particles( char *file_name, float *x_pos, float *y_pos, float *z_pos )
 {
   FILE *file = fopen( file_name, "r" );
