f307b7e9f0213218bb1b35b09a0deafe59720045
geoff.ulman <geoff.ulman@20f29d02-21cd-11df-99a0-0961c9d9be7b>
2010-03-20 00:48:01 +0000

    homework4: started process of creating an additional comm group to handle circular message passing.

--- branches/homework4/src/problem3/nbody_parallel.c
+++ branches/homework4/src/problem3/nbody_parallel.c
@@ -9,13 +9,34 @@
 
 int main( int argc, char** argv )
 {
-  int numprocs, myid;
+  int numprocs, myid, mycartid;
 
   MPI_Init( &argc, &argv );
   MPI_Comm_size( MPI_COMM_WORLD, &numprocs );
   MPI_Comm_rank( MPI_COMM_WORLD, &myid );
 
-  printf( "Hello world %d %d\n", numprocs, myid );
+  printf( "id %d of %d\n", myid, numprocs );
+
+  MPI_Comm cart_comm;
+
+  // create a 1 dimensional periodic cartesian grid
+  // with numprocs nodes and store in cart_comm
+  MPI_Cart_create( MPI_COMM_WORLD, 1, numprocs, 1, 1, &cart_comm );
+
+  // get rank in the new comm group
+  MPI_Comm_rank( cart_comm, &mycartid );
+  
+  int source, topid, bottomid;
+
+  // get our top neighbor
+  MPI_Cart_shift( cart_comm, 0, 1, &source, &topid );
+
+  // get our bottom neighbor
+  MPI_Cart_shift( cart_comm, 0, -1, &source, &bottomid );  
+
+  void * buf = malloc( sizeof(int) );
+
+  printf( "id %d top neighbor %d bottom neighbor %d\n", source, topid, bottomid );
 
   MPI_Finalize();
 
