ae93d2625a122673047c735d64eb7441e1278c68
geoff.ulman <geoff.ulman@20f29d02-21cd-11df-99a0-0961c9d9be7b>
2010-03-21 03:29:57 +0000

    problem4: started implementation of parallel sort.

--- branches/homework4/problem4/src/sort_parallel.c
+++ branches/homework4/problem4/src/sort_parallel.c
@@ -8,5 +8,46 @@
 
 int main( int argc, char** argv )
 {
+  int numprocs, myid;
 
+  // initialize mpi
+  MPI_Init( &argc, &argv );
+  MPI_Comm_size( MPI_COMM_WORLD, &numprocs );
+  MPI_Comm_rank( MPI_COMM_WORLD, &myid );
+
+  //printf( "id %d of %d\n", myid, numprocs );
+
+  MPI_Request *req = (MPI_Request *) malloc( sizeof(MPI_Request) * numprocs );
+  MPI_Status *stat = (MPI_Status *) malloc( sizeof(MPI_Status) * numprocs );
+
+  int values_per_proc = ARRAY_SIZE / numprocs;
+  int *values = (int *) malloc( sizeof(int) * values_per_proc );
+
+  if ( myid == 0 )
+  {
+    // set the random seed and generate the random array
+    srand( time( NULL ) );
+    int *all_values = generate_random_array( ARRAY_SIZE , 100000000 );
+
+    // if there are any extra values, they go on node 0
+    int extra_values = ARRAY_SIZE % numprocs;
+
+    // send sections of the array to each proc
+    int i;
+    for ( i = i ; i < numprocs ; i++ )
+    {
+      int index = i * values_per_proc + extra_values;
+      MPI_Isend( all_values + index , values_per_proc, MPI_INTEGER, i, 1, MPI_COMM_WORLD, req + i - 1 );
+    }
+    
+    // wait for all the nodes to receive their data
+    MPI_Waitall( numprocs-1, req, stat );
+  }
+  else
+  {
+    // listen for our portion of the array being sent from node 0
+    MPI_Recv( values, values_per_proc, MPI_INTEGER, 0, 1, MPI_COMM_WORLD, stat );
+  }
+
+  printf( "id %d of %d\n", myid, numprocs ); 
 }
