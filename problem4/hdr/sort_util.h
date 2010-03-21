61ec3b944211b14798ecc16d300d058a7cbf9ad7
geoff.ulman <geoff.ulman@20f29d02-21cd-11df-99a0-0961c9d9be7b>
2010-03-21 01:19:27 +0000

    homework4: begain stubbing out utility methods for problem4

--- problem4/hdr/sort_util.h
+++ problem4/hdr/sort_util.h
@@ -1,5 +1,9 @@
 #ifndef SORT_UTIL_H_INCLUDED
 #define SORT_UTIL_H_INCLUDED
 
+#define ARRAY_SIZE 10
+
+int *generate_random_array( int );
+void print_array( int, int* );
 
 #endif
--- problem4/src/sort_serial.c
+++ problem4/src/sort_serial.c
@@ -4,7 +4,35 @@
 
 #include "sort_util.h"
 
+void serial_sort( int*, int , int , int (*)( int, int ) );
+int compare_integers( int , int );
+
 int main( int argc, char** argv )
 {
+  int *values = generate_random_array( ARRAY_SIZE );
 
+  print_array( ARRAY_SIZE, values );
+
+  serial_sort( values, 0, ARRAY_SIZE-1, (int (*)( int , int )) compare_integers );
+
+  free( values );
+}
+
+// "The C Programming Language", Brian W. Kernighan and Dennis M. Ritchie used as reference for
+// quicksort implementaion (mainly for puzzling out the types necessary to make the implementation
+// general enough to support any comparison function).
+void serial_sort( int* values, int min, int max, int (*comp)( int , int ) )
+{
+  int i;
+  for ( i = min ; i <= max-1 ; i++ )
+  {
+    printf("value %d comp %d\n", values[i], comp( values[i] , values[i+1] ) );
+  } 
+}
+
+int compare_integers( int i1, int i2 )
+{
+  if ( i1 < i2 ) return -1;
+  else if ( i1 > i2) return 1;
+  else return 0;
 }
