c5d749d2d87d2eb1026eae0bde4799b68e94a9e4
geoff.ulman <geoff.ulman@20f29d02-21cd-11df-99a0-0961c9d9be7b>
2010-03-21 01:19:27 +0000

    homework4: begain stubbing out utility methods for problem4

--- branches/homework4/problem4/src/sort_util.c
+++ branches/homework4/problem4/src/sort_util.c
@@ -1,3 +1,32 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
+
+int *generate_random_array( int size )
+{
+  int* r = (int *) malloc( sizeof(int) * size );
+  
+  int i;
+  for ( i = 0 ; i < size ; i++ )
+  {
+    r[i] = rand( );
+  }
+
+  return r;
+}
+
+void print_array( int size, int *values )
+{
+  int i;
+  for ( i = 0 ; i < size ; i++ )
+  {
+    printf( "%d\n", values[i] );
+  }
+}
+
+void swap( int* a, int* b )
+{
+  int temp = *a;
+  *a = *b;
+  *b = temp;
+}
