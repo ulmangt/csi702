52cafdd29cd033f0e1f41b15593d8fe769ce93b6
geoff.ulman <geoff.ulman@20f29d02-21cd-11df-99a0-0961c9d9be7b>
2010-03-22 01:14:49 +0000

    Finished barrier code using condition variable.

--- src/condition_barrier.c
+++ src/condition_barrier.c
@@ -7,9 +7,33 @@
 pthread_mutex_t mutex;
 pthread_cond_t cond;
 
+int counter;
+
+int randn( int );
+
 void *run( int *index )
 {
-  printf( "id %d\n", *index );
+  printf( "index %d started\n", *index );
+
+  // do work
+  sleep( randn( 10 ) );
+
+  pthread_mutex_lock( &mutex );
+
+  if ( --counter == 0 )
+  {
+    printf( "index %d broadcasting counter: %d\n", *index, counter );
+    pthread_cond_broadcast( &cond );
+  }
+  else
+  {
+    printf( "index %d waiting counter: %d\n", *index, counter );
+    pthread_cond_wait( &cond, &mutex );    
+  }
+
+  pthread_mutex_unlock( &mutex );
+
+  printf( "index %d done\n", *index );
 }
 
 int main( int argc, char** argv )
@@ -22,6 +46,8 @@ int main( int argc, char** argv )
 
   // initialize condition variable
   pthread_cond_init( &cond, NULL );
+
+  counter = NUM_THREADS;
 
   pthread_t threads[NUM_THREADS];
   int thread_ids[NUM_THREADS];
@@ -42,4 +68,9 @@ int main( int argc, char** argv )
   pthread_mutex_destroy( &mutex );
   pthread_cond_destroy( &cond );
   pthread_exit( NULL );
+}
+
+int randn( int n )
+{
+  return rand( ) % n;
 }
