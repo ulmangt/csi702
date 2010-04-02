2434eb969fc1c6dae69b911ed170d8da1d0fa752
geoff.ulman <geoff.ulman@20f29d02-21cd-11df-99a0-0961c9d9be7b>
2010-04-02 01:35:37 +0000

    homework4: updated comments, cleaned up code, and updated reports.

--- branches/homework4/problem4/src/sort_serial.c
+++ branches/homework4/problem4/src/sort_serial.c
@@ -6,14 +6,17 @@
 
 int main( int argc, char** argv )
 {
+  // initialize the random seed
   srand( time( NULL ) );
 
   // read data to sort from a file, the commented line generates data randomly
   int *values = read_array( UNSORTED_NAME, ARRAY_SIZE );
   //int *values = generate_random_array( ARRAY_SIZE, MAX_VALUE );
 
+  // binary sort the values array
   serial_sort( values, 0, ARRAY_SIZE-1, (int (*)( int , int )) compare_integers );
 
+  // verify that the list has been sorted
   int check = check_sorted( ARRAY_SIZE , values );
   if ( check < 0 )
   {
