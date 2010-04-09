713bd611236628e38344d606058ce608c5dd50db
geoff.ulman <geoff.ulman@20f29d02-21cd-11df-99a0-0961c9d9be7b>
2010-04-09 17:43:33 +0000

    final_project: further added to the mess with more stripped down test file. I'm slowly working out the compilation issues.

---
+++ hdr/cuda_test.h
@@ -0,0 +1,9 @@
+
+struct particles {
+float x_pos; // meters
+float y_pos; // meters
+float x_vel; // meters/second
+float y_vel; // meters/second
+float weight;
+float seed; // random seed for particle
+};
