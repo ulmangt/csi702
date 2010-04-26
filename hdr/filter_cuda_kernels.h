a9b2996e13ee7a0bb4a5b1c0d96f01e0599f05eb
geoff.ulman <geoff.ulman@20f29d02-21cd-11df-99a0-0961c9d9be7b>
2010-04-26 02:40:52 +0000

    final_project: modifed filter_cuda_kernel.cu to use structs of arrays passed by value

--- hdr/filter_cuda_kernels.h
+++ hdr/filter_cuda_kernels.h
@@ -5,17 +5,17 @@
 #include "obs_math.h"
 #include "filter_cuda_data.h"
 
-extern void init_particles( struct particles *device_particles, int num );
+extern void init_particles( struct particles device_particles, int num );
 
 extern float sum_weight( float *d_weights, float *d_temp_weight1, float *d_temp_weight2, int num );
 
 extern float sum_weight_thrust( float *d_weights, int num );
 
-extern void information_update( struct observation *obs, struct particles *device_particles, int num );
+extern void information_update( struct observation obs, struct particles device_particles, int num );
 
-extern void time_update( struct particles *device_particles, int num, float time_sec, float mean_maneuver );
+extern void time_update( struct particles device_particles, int num, float time_sec, float mean_maneuver );
 
-extern void resample( struct particles *device_particles, struct particles *device_particles_swap, int num );
+extern void resample( struct particles device_particles, struct particles device_particles_swap, int num );
 
 extern void init_array( float *array, float value, int num );
 
